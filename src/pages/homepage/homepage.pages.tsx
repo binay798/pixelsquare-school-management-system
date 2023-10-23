939b3483dc8c438bd6635dd348789b5bbd29aa9b
Binay shrestha <binayshrestha798@gmail.com>
2023-10-23 12:39:44 +0545

    fix: removed button component at homepage

--- src/pages/homepage/homepage.pages.tsx
+++ src/pages/homepage/homepage.pages.tsx
@@ -1,24 +1,11 @@
 import { Typography } from '@mui/material'
-import { TooltipBtn } from '@src/components/button/button.component'
-import { useState } from 'react'
 
 export function Homepage() {
-  const [load, setLoad] = useState(false)
-
   return (
     <div>
       <Typography>Homepage</Typography>
 
-      <div style={{ padding: 10 }}>
-        <TooltipBtn
-          variant="contained"
-          tooltipTitle="open modal"
-          loading={load}
-          onClick={() => setLoad((prev) => !prev)}
-        >
-          Open modal se
-        </TooltipBtn>
-      </div>
+      <div style={{ padding: 10 }}></div>
     </div>
   )
 }
